1b89af0d8f08da89449fff9d009e8a92f12b7b88
Thomas Günzel <thomas.guenzel@tum.de>
2015-05-29 13:12:28 +0200

    added xcode project

---
+++ RecommenderSystemExample/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RecommenderSystemExample
+//
+//  Created by Thomas Günzel on 29/05/15.
+//  Copyright (c) 2015 Thomas Günzel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+    return 0;
+}
